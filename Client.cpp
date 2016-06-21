9f5a7c156b4ac70cfb155709f0d86cd7fca6056d
Midgardsorm <chrusciel.krzysztof@gmail.com>
2016-06-21 16:53:48 +0200

    Klasa Server i Klient, funkcja msg - cała funkcjonalność usunięta z main()

--- Client.cpp
+++ Client.cpp
@@ -6,13 +6,80 @@
 #include <winsock2.h>
 
 
-
+//klasa zawierajπca dane przesy≥ane do watkow przez wskaünik
 class Data {
 public:
 	SOCKET mainSocket;
 	char buffer[1000];
 };
 
+//klasa dla funkcji inicjalizujπcych i obslugujacych gniazda
+class Client {
+private:
+	const char *m_ip;
+	int m_port;
+	WSADATA m_wsaData;
+	Data *mainData;
+	sockaddr_in service;
+public:
+	Client(Data *data) : m_ip("127.0.0.1"), m_port(27015), mainData(data)
+	{
+
+	}
+
+	~Client()
+	{
+		std::cout << "Zamkniecie gniazda.";
+	}
+
+	int winsockInit();
+	int socketInit();
+	void adressInit();
+	int serverConnect();
+};
+
+int Client::winsockInit()
+{
+	int result = WSAStartup(MAKEWORD(2, 2), &m_wsaData);
+	if (result != NO_ERROR)
+	{
+		std::cout << "Nie moøna zainicjowac gniazda.";
+		return 1;
+	}
+	return 0;
+}
+
+int Client::socketInit()
+{
+	mainData->mainSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (mainData->mainSocket == INVALID_SOCKET)
+	{
+		std::cout << "Blad przy tworzeniu gniazda: %ld\n" << WSAGetLastError();
+		WSACleanup();
+		return 1;
+	}
+	return 0;
+}
+
+void Client::adressInit()
+{
+	memset(&service, 0, sizeof(service));
+	service.sin_family = AF_INET;
+	service.sin_addr.s_addr = inet_addr(m_ip);
+	service.sin_port = htons(m_port);
+}
+
+int Client::serverConnect()
+{
+	int connected = connect(mainData->mainSocket, (SOCKADDR *)& service, sizeof(service));
+	if (connected == SOCKET_ERROR)
+	{
+		std::cout << "Nieudane podlaczenie.\n";
+		WSACleanup();
+		return 1;
+	}
+	return 0;
+}
 
 void listenForMsg(void * mainData)
 {
@@ -24,62 +91,40 @@ void listenForMsg(void * mainData)
 		{
 			bytesRecv = recv(someData->mainSocket, someData->buffer, 1000, 0);
 		}
-		std::cout << "Server: " << someData->buffer << "\n";
+		std::cout << "Serwer: " << someData->buffer << "\n";
 	} while (1);
 
 	_endthread();
+}
+
+void sendMsg(Data *mainData)
+{
+	do {
+		std::cin.getline(mainData->buffer, 1000);
+		send(mainData->mainSocket, mainData->buffer, 1000, 0);
+	} while (*(mainData->buffer) != '#');
+
+	std::cout << "Koniec transmisji.\n";
 }
 
 int main()
 {
 	Data* mainData = new Data;
 	
-	//inicjalizacja winsocka
-	WSADATA wsaData;
+	Client client(mainData);
 
-	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (result != NO_ERROR)
-	{
-		std::cout << "Nie moøna zainicjowaÊ gniazda";
-	}
+	client.winsockInit();
+	client.socketInit();
+	client.adressInit();
+	client.serverConnect();
 
-	//inicjalizacja gniazda
-	mainData->mainSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (mainData->mainSocket == INVALID_SOCKET)
-	{
-		std::cout << "Error creating socket: %ld\n" << WSAGetLastError();
-		WSACleanup();
-		return 1;
-	}
 	
-	//podanie ip i portu
-	sockaddr_in service;
-	memset(&service, 0, sizeof(service));
-	service.sin_family = AF_INET;
-	service.sin_addr.s_addr = inet_addr("127.0.0.1");
-	service.sin_port = htons(27015);
-
-	//podlaczanie do serwera
-	int connected = connect(mainData->mainSocket, (SOCKADDR *)& service, sizeof(service));
-	if (connected == SOCKET_ERROR)
-	{
-		printf("Failed to connect.\n");
-		WSACleanup();
-		return 1;
-	}
-
-		
 	//watki:
 	//nasluchuje wiadomosci od podlaczonych klientow
 	_beginthread(listenForMsg, 0, mainData);
 	
 	//wysylanie wiadomosci i obs≥uga chatu
-	do {
-		std::cin.getline(mainData->buffer, 1000);
-		send(mainData->mainSocket, mainData->buffer, 1000, 0);
-	} while (*(mainData->buffer) != '#');
-
-	std::cout << "Ending trasmission ... \n";
+	sendMsg(mainData);
 
 	//czyszczenie pamieci
 	delete mainData;
--- Server.cpp
+++ Server.cpp
@@ -6,12 +6,107 @@
 #include <winsock2.h>
 
 
-
+//klasa zawierajaca dane przesylane do watkow przez wskaznik
 class Data {
 public:
 	SOCKET mainSocket;
 	char buffer[1000];
 };
+
+//klasa dla funkcji inicjalizuj¹cych i obslugujacych gniazda
+class Server {
+private:
+	const char *m_ip;
+	int m_port;
+	WSADATA m_wsaData;
+	Data *mainData;
+	sockaddr_in service;
+public:
+	Server(Data *data) : m_ip("127.0.0.1"), m_port(27015), mainData(data)
+	{
+
+	}
+
+	~Server()
+	{
+		std::cout << "Zamkniecie gniazda.";
+	}
+	
+	int winsockInit();
+	int socketInit();
+	void adressInit();
+	int bindInit();
+	void listenInit();
+	void clientInit();
+};
+
+int Server::winsockInit()
+{
+	int result = WSAStartup(MAKEWORD(2, 2), &m_wsaData);
+	if (result != NO_ERROR)
+	{
+		std::cout << "Nie mozna zainicjowac gniazda.";
+		return 1;
+	}
+	return 0;
+}
+
+int Server::socketInit()
+{
+	mainData->mainSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (mainData->mainSocket == INVALID_SOCKET)
+	{
+		std::cout << "Blad przy tworzeniu gniazda: %ld\n" << WSAGetLastError();
+		WSACleanup();
+		return 1;
+	}
+	return 0;
+}
+
+void Server::adressInit()
+{
+	memset(&service, 0, sizeof(service));
+	service.sin_family = AF_INET;
+	service.sin_addr.s_addr = inet_addr(m_ip);
+	service.sin_port = htons(m_port);
+}
+
+int Server::bindInit()
+{
+	int binding = bind(mainData->mainSocket, (SOCKADDR *)& service, sizeof(service));
+	if (binding == SOCKET_ERROR)
+	{
+		std::cout << "Przypisanie adresu bind() nieudane.\n";
+		closesocket(mainData->mainSocket);
+		return 1;
+	}
+	return 0;
+}
+
+
+void Server::listenInit()
+{
+	int listening = listen(mainData->mainSocket, 1);
+	if (listening == SOCKET_ERROR)
+	{
+		std::cout << "Blad nasluchu na gniezdzie.\n";
+	}
+}
+
+
+void Server::clientInit()
+{
+	SOCKET acceptSocket = SOCKET_ERROR;
+	std::cout << "Oczekiwanie na klienta\n";
+
+	while (acceptSocket == SOCKET_ERROR)
+	{
+		acceptSocket = accept(mainData->mainSocket, NULL, NULL);
+	}
+
+	std::cout << "Klient podlaczony.\n";
+	mainData->mainSocket = acceptSocket;
+}
 
 void listenForMsg(void * mainData)
 {
@@ -23,69 +118,34 @@ void listenForMsg(void * mainData)
 		{
 			bytesRecv = recv(someData->mainSocket, someData->buffer, 1000, 0);
 		}
-		std::cout << "Client: " << someData->buffer << "\n";
+		std::cout << "Klient: " << someData->buffer << "\n";
 	} while (1);
 	
 	_endthread();
 }
 
+void sendMsg(Data *mainData)
+{
+	do {
+		std::cin.getline(mainData->buffer, 1000);
+		send(mainData->mainSocket, mainData->buffer, 1000, 0);
+	} while (*(mainData->buffer) != '#');
+
+	std::cout << "Koniec transmisji.\n";
+}
 
 int main()
 {
 	Data* mainData = new Data;
 	
-	//inicjalizacja winsocka
-	WSADATA wsaData;															
+	Server server(mainData);
 
-	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (result != NO_ERROR)
-	{
-		std::cout << "Nie mo¿na zainicjowaæ gniazda";
-	}
-
-	//inicjalizacja gniazda
-	mainData->mainSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (mainData->mainSocket == INVALID_SOCKET)
-	{
-		std::cout << "Error creating socket: %ld\n" << WSAGetLastError();
-		WSACleanup();
-		return 1;
-	}
-
-	//podanie ip i portu
-	sockaddr_in service;
-	memset(&service, 0, sizeof(service));
-	service.sin_family = AF_INET;
-	service.sin_addr.s_addr = inet_addr("127.0.0.1");
-	service.sin_port = htons(27015);
-
-	//bindowanie adresu i portu do gniazda
-	int binding = bind(mainData->mainSocket, (SOCKADDR *)& service, sizeof(service));
-	if (binding == SOCKET_ERROR)
-	{
-		std::cout << "bind() failed.\n";
-		closesocket(mainData->mainSocket);
-		return 1;
-	}
-
-	//wlaczenie funkcji nasluchowej
-	int listening = listen(mainData->mainSocket, 1);
-	if (listening == SOCKET_ERROR)
-	{
-		std::cout << "Error listening on socket.\n";
-	}
-	
-	//podlaczenie klienta i nasluchiwanie
-	SOCKET acceptSocket = SOCKET_ERROR;
-	std::cout << "Waiting for a client to connect...\n";
-
-	while (acceptSocket == SOCKET_ERROR)
-	{
-		acceptSocket = accept(mainData->mainSocket, NULL, NULL);
-	}
-
-	std::cout << "Client connected.\n";
-	mainData->mainSocket = acceptSocket;
+	server.winsockInit();
+	server.socketInit();
+	server.adressInit();
+	server.bindInit();
+	server.listenInit();
+	server.clientInit();
 
 	
 	//watki:
@@ -93,12 +153,7 @@ int main()
 	_beginthread(listenForMsg, 0, mainData);
 
 	//wysylanie wiadomosci i obs³uga chatu
-	do {
-		std::cin.getline(mainData->buffer, 1000);
-		send(mainData->mainSocket, mainData->buffer, 1000, 0);
-	} while (*(mainData->buffer) != '#');
-
-	std::cout << "Ending trasmission ... \n";
+	sendMsg(mainData);
 
 	//czyszczenie pamieci
 	delete mainData;
